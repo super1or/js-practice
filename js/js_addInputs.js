6175c5084ec14b4f575463f140fd77a7879a4fb8
super1or <38477985+super1or@users.noreply.github.com>
2018-10-23 12:39:58 +0300

    first commit

---
+++ js/js_addInputs.js
@@ -0,0 +1,12 @@
+// Onclick event
+
+// function fun() {
+//     alert("you have pressed the button");
+// }
+
+
+// Working with input attribute 
+
+function quad() {
+    alert(document.getElementById("int").value*document.getElementById("int").value);
+}
